5b852df14d051e74d05275e3eb8bbd9b063cb50c
tortenotorteno <jackwilson3249@gmail.com>
2025-05-28 16:56:52 -0400

    added maven compatibility
    issues with images

---
+++ src/main/java/com/example/pumpkinquest/configue.java
@@ -0,0 +1,4 @@
+package com.example.pumpkinquest;
+
+public class configue {
+}
---
+++ src/main/java/com/example/pumpkinquest/panel.java
@@ -0,0 +1,20 @@
+package com.example.pumpkinquest;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+
+class BackgroundPanel extends JPanel {
+    private Image backgroundImage;
+
+    public BackgroundPanel(String fileName) {
+        backgroundImage = new ImageIcon(fileName).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+}
